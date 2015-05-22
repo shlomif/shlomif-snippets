951ebf9f6dd4f99242e049f56f7d242c94b1b30f
Shlomi Fish <shlomif@shlomifish.org>
2015-05-22 12:34:55 +0300

    Primes at rough various exponential interval.
    From what I recall it was originally written for Freecell Solver’s hash
    where we were recommende to use a prime a bit bigger than a power of 2.

---
+++ primes-rough-geometric-series/gen_prime_list.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use autodie;
+
+my @a;
+open my $in_fh, '<', 'prime_list.txt';
+while (my $l = <$in_fh>)
+{
+    if ($l =~ /\d/)
+    {
+        chomp($l);
+        push @a, $l;
+    }
+}
+close($in_fh);
+
+push @a, "-1";
+
+print "primes[" . scalar(@a) . "] = {\n";
+print join(",\n", (map { "    ".$_; } @a));
+print "\n};\n";
---
+++ primes-rough-geometric-series/primes.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+# use autodie;
+
+use Math::BigInt lib => 'GMP', ':constant';
+
+STDOUT->autoflush(1);
+
+my $start_int = 256;
+my $end_int = 0x80000000;
+
+sub next_number
+{
+    my $this = shift;
+
+    return ($this*9/8);
+}
+
+my $i = $start_int;
+
+while ($i < $end_int)
+{
+    open my $p_fh, "-|", "primes", "$i"
+        or die "Cannot open primes - $!";
+    print scalar <$p_fh>;
+    close ($p_fh);
+}
+continue
+{
+    $i = next_number($i);
+}
+
+
