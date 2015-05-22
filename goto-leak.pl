77ab4f05b8fee485e0ac36d0aca76c433da0a244
Shlomi Fish <shlomif@shlomifish.org>
2015-05-22 11:15:20 +0300

    Add goto-leak.
    This was reported to rt.perl.org recently.

---
+++ goto-leak.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+sub naturals {
+    my ($n)= @_;
+    sub {
+        if ($n > 0) {
+            [ $n, naturals($n - 1) ]
+        } else {
+            undef
+        }
+    }
+}
+
+sub stream_sum {
+    my ($s, $tot)=@_;
+    if (my $fs= &$s) {
+        @_=($$fs[1], $$fs[0] + $tot); goto \&stream_sum;
+    } else {
+        $tot
+    }
+}
+
+my $ns= naturals 200000;
+<STDIN>;
+my $res= stream_sum ($ns, 0);
+<STDIN>;
