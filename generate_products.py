6e79192c986e81799c6f14745fa0f3d32d75e57e
Shlomi Fish <shlomif@shlomifish.org>
2017-12-23 19:25:11 +0200

    Add more.

---
+++ generate_products.py
@@ -0,0 +1,35 @@
+# The Expat License
+#
+# Copyright (c) 2017, Shlomi Fish
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+
+def gen_products(lst, idx, prod):
+    if idx == len(lst):
+        yield prod
+        return
+    for x in gen_products(lst, idx+1, prod):
+        yield x
+    for x in gen_products(lst, idx+1, prod*lst[idx]):
+        yield x
+
+
+for x in gen_products([2, 3, 5, 7], 0, 1):
+    print(x)
