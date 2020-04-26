cd37a175e2e555a94e57a843ccb61f71d1b7c39c
Shlomi Fish <shlomif@shlomifish.org>
2020-04-26 12:36:00 +0300

    scale

--- plot_mandelbrot.py
+++ plot_mandelbrot.py
@@ -26,17 +26,24 @@ def compute_mandelbrot(N_max, some_threshold, nx, ny):
     c = x[:, newaxis] + 1j*y[newaxis, :]
 
     # Mandelbrot iteration
-
+    ret = np.zeros([nx, ny], dtype=np.int32)
+    mask = ret * 0
     z = c
     for j in range(N_max):
         z = z**2 + c
+        where = (abs(z) > some_threshold)
+        z *= (np.logical_not(where))
+        mask = np.logical_or(mask, where)
+        ret += mask
+        print(np.where(ret == j-1))
+    return ret
 
     mandelbrot_set = (abs(z) < some_threshold)
 
     return mandelbrot_set
 
 
-mandelbrot_set = compute_mandelbrot(50, 50., 601, 401)
+mandelbrot_set = compute_mandelbrot(256, 9., 601, 401) / 256
 
 plt.imshow(mandelbrot_set.T, extent=[-2, 1, -1.5, 1.5])
 plt.gray()
