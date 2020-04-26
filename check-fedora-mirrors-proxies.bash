956ab1d1ad30f740d416c50ca379a0ccf35613bf
Shlomi Fish <shlomif@shlomifish.org>
2020-04-26 17:28:03 +0300

    use lowercase idents for non-exported

--- check-fedora-mirrors-proxies.bash
+++ check-fedora-mirrors-proxies.bash
@@ -3,21 +3,21 @@
 # See:
 # https://pagure.io/fedora-infrastructure/issue/8865
 
-PROXIES='proxy01.fedoraproject.org proxy02.fedoraproject.org proxy03.fedoraproject.org proxy04.fedoraproject.org proxy05.fedoraproject.org proxy06.fedoraproject.org proxy09.fedoraproject.org proxy10.fedoraproject.org proxy11.fedoraproject.org proxy12.fedoraproject.org proxy13.fedoraproject.org proxy14.fedoraproject.org proxy30.fedoraproject.org proxy31.fedoraproject.org'
+proxies_list='proxy01.fedoraproject.org proxy02.fedoraproject.org proxy03.fedoraproject.org proxy04.fedoraproject.org proxy05.fedoraproject.org proxy06.fedoraproject.org proxy09.fedoraproject.org proxy10.fedoraproject.org proxy11.fedoraproject.org proxy12.fedoraproject.org proxy13.fedoraproject.org proxy14.fedoraproject.org proxy30.fedoraproject.org proxy31.fedoraproject.org'
 
-URLS="https://mirrors.fedoraproject.org/metalink?repo=updates-released-f32&arch=x86_64 https://mirrors.fedoraproject.org/metalink?repo=fedora-32&arch=x86_64"
+urls_list="https://mirrors.fedoraproject.org/metalink?repo=updates-released-f32&arch=x86_64 https://mirrors.fedoraproject.org/metalink?repo=fedora-32&arch=x86_64"
 
 
-for PROXY in ${PROXIES} ;
+for proxy in ${proxies_list} ;
 do
-    for URL in ${URLS} ;
+    for url in ${urls_list} ;
     do
-        echo -n "${PROXY}: "
-        curl --connect-to mirrors.fedoraproject.org:443:${PROXY}:443 "${URL}" &> /dev/null
+        echo -n "${proxy}: "
+        curl --connect-to mirrors.fedoraproject.org:443:${proxy}:443 "${url}" &> /dev/null
         if [[ ${?} -eq 0 ]]; then
-            echo "mirror ${URL} is working"
+            echo "mirror ${url} is working"
         else
-            echo "mirror ${URL} is not working"
+            echo "mirror ${url} is not working"
         fi
     done
 done
