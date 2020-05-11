a071bbb1b04a873b7f6b3a5dd53724a786b03073
Shlomi Fish <shlomif@shlomifish.org>
2020-05-11 12:07:43 +0300

    refactor

--- sha-256-and-len.pl
+++ sha-256-and-len.pl
@@ -5,16 +5,47 @@ use warnings;
 
 use bytes;
 
-use Digest::SHA;
+use Digest ();
 
-my $sha = Digest::SHA->new(256);
+my $sha = Digest->new('SHA-256');
 
 my $data;
 my $len = 0;
-while (read(*STDIN, $data, 32768))
+while ( read( *STDIN, $data, 32768 ) )
 {
     $sha->add($data);
     $len += length($data);
 }
 print "Len: $len\nDigest: " . $sha->hexdigest() . "\n";
 
+__END__
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2013 by Shlomi Fish
+
+This program is distributed under the MIT / Expat License:
+L<http://www.opensource.org/licenses/mit-license.php>
+
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without
+restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following
+conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+=cut
