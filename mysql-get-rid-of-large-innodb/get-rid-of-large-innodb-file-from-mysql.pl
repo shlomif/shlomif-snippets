5b42a23c36a049892b3e7a83a896534e9f09dfed
Shlomi Fish <shlomif@shlomifish.org>
2020-05-18 15:57:29 +0300

    Refactoring / code cleanup.
    See:
    
    * https://en.wikipedia.org/wiki/Code_refactoring
    
    * https://www.refactoring.com/
    
    * https://www.joelonsoftware.com/2002/01/23/rub-a-dub-dub/
    
    Some small optimisations may have slipped in as well.

--- mysql-get-rid-of-large-innodb/get-rid-of-large-innodb-file-from-mysql.pl
+++ mysql-get-rid-of-large-innodb/get-rid-of-large-innodb-file-from-mysql.pl
@@ -2,31 +2,62 @@
 
 use strict;
 use warnings;
-use JSON::XS;
+use JSON::MaybeXS qw/ encode_json /;
 
-open my $in, "-|", "mysql", "-u", "root", "information_schema", "-e", "SELECT TABLE_SCHEMA,TABLE_NAME FROM TABLES WHERE ENGINE = 'InnoDB';"
+use Path::Tiny qw/ path tempdir tempfile cwd /;
+
+open my $in, "-|", "mysql", "-u", "root", "information_schema", "-e",
+    "SELECT TABLE_SCHEMA,TABLE_NAME FROM TABLES WHERE ENGINE = 'InnoDB';"
     or die "Could not open mysql client!";
 
-my $cnt = 0;
+my $cnt     = 0;
 my $headers = <$in>;
 
 my %dbs;
 while (<$in>)
 {
     chomp;
-    my ($db, $table) = split(/\s+/, $_);
-    push @{$dbs{$db}}, $table;
+    my ( $db, $table ) = split( /\s+/, $_ );
+    push @{ $dbs{$db} }, $table;
 }
 close($in);
 
+path("mysql-innodb-tables.json")->spew_utf8( encode_json( \%dbs ) );
+
+while ( my ( $db, $tables ) = each(%dbs) )
 {
-    open my $out, ">", "mysql-innodb-tables.json"
-        or die "Cannot open dump file for writing.";
-    print {$out} encode_json(\%dbs);
-    close($out);
+    system( "mysql", "-u", "root", $db, "-e",
+        join( " ", map { "ALTER TABLE $_ ENGINE=MyISAM;" } @$tables ) );
 }
 
-while (my ($db , $tables) = each(%dbs))
-{
-    system("mysql", "-u", "root", $db, "-e", join(" ", map { "ALTER TABLE $_ ENGINE=MyISAM;" } @$tables));
-}
+__END__
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2020 by Shlomi Fish
+
+This program is distributed under the MIT / Expat License:
+L<http://www.opensource.org/licenses/mit-license.php>
+
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without
+restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following
+conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+=cut
