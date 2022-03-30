4f73c7078da28c97308b1cb4900b80d55adbd775
Shlomi Fish <shlomif@shlomifish.org>
2022-03-30 08:10:56 +0300

    Refactoring / code cleanup.
    See:
    
    * https://en.wikipedia.org/wiki/Code_refactoring
    
    * https://www.refactoring.com/
    
    * https://www.joelonsoftware.com/2002/01/23/rub-a-dub-dub/
    
    Some small optimisations may have slipped in as well.

--- plot_mandelbrot_cc0.py
+++ plot_mandelbrot_cc0.py
@@ -11,15 +11,13 @@ import png
 
 
 # Assign some default values to the parameters
-# nargin is an internal Matlab variable that specifies how many
-# parameters were passed to the function.
-def mandel(x=640, y=640, steps=20, init_value=0, max_level=255):
+def mandel(x=640, y=640, num_steps=20, init_value=0, max_level=255):
     xx = np.linspace(-2, 2, x)
     yy = np.linspace(-2, 2, y)
     # Generate the coordinates in the complex plane
     X, Y = np.meshgrid(xx, yy, indexing='ij')
     # Combine them into a matrix of complex numbers
-    Z = X+1j*Y
+    Z = X + 1j * Y
     # Retrieve the dimensions of Z
     x_len, y_len = zs = Z.shape
     # The length in the x direction
@@ -33,36 +31,35 @@ def mandel(x=640, y=640, steps=20, init_value=0, max_level=255):
     # to zero, to indicate that none have so far.
     mask = np.zeros(zs, dtype=bool)
 
-    # Perform the check "steps" times
-    for step in range(steps):
+    # Perform the check "num_steps" times
+    for step in range(num_steps):
         # For every point with a mandel value of "v" and a coordinate of "z"
         # perform  v <- (v ^ 2) + z
         #
-        # .* is an element-by-element multiplication of two matrixes
+        # * is an element-by-element multiplication of two matrixes
         # of the same size.
-        # (regular * indicates matrix multiplication)
         value = (value * value) + Z
         # assert value.any()
         # Retrieve the points that overflowed in this iteration
         # An overflowed point has a mandel value with an absolute value greater
         # than 2.
-        current_mask = (abs(value) > 2)
+        current_mask = (abs(value) > 2.0)
         # Update the mask. We use "or" in order to avoid a situation where
         # 1 and 1 become two or so.
         mask = np.logical_or(mask, current_mask)
         # Upgrade the points in the mask to a greater value in the returned
         # Mandelbrot-map.
-        ret = ret + mask
+        ret += mask
         # Zero the points that have overflowed, so they will not propagate
         # to infinity.
-        value = value * np.logical_not(current_mask)
+        value *= np.logical_not(current_mask)
 
     # Now ret is ready for prime time so we return it.
-    ret = ret * max_level // steps
+    ret = (ret * max_level) // num_steps
     return ret
 
 
-mandelbrot_set = mandel(max_level=255, steps=255)
+mandelbrot_set = mandel(max_level=255, num_steps=255)
 mandelbrot_set = mandelbrot_set.astype('uint8')
 m = np.repeat(mandelbrot_set, 3, axis=1)
 greyscale_fn = "mandel.png"
@@ -77,12 +74,14 @@ subprocess.check_call(
         "--no-interface",
         "--batch-interpreter=python-fu-eval",
         "-b",
-        ('img = gimp.image_list()[0]\ndraw=img.active_drawable\n' +
+        ('img = gimp.image_list()[0]\n' +
+         'draw=img.active_drawable\n' +
          'pdb.gimp_context_set_gradient("Tropical Colors")\n' +
          'pdb.plug_in_gradmap(img, draw)\n' +
          'pdb.gimp_file_save(img, draw, "{colored_fn}", "{colored_fn}")\n' +
          'pdb.gimp_quit(1)\n'
          ).format(colored_fn=colored_fn)
-    ])
+    ]
+)
 
 subprocess.check_call(["gwenview", colored_fn])
