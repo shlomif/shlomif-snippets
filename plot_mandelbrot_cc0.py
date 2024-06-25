8755e22de7079f2a9d51cd897accdf58b85e91c1
Shlomi Fish <shlomif@shlomifish.org>
2024-06-25 17:20:07 +0300

    add the python3 integer-based mandelbrot set calculation routine
    https://en.wikipedia.org/wiki/Mandelbrot_set

--- plot_mandelbrot_cc0.py
+++ plot_mandelbrot_cc0.py
@@ -61,8 +61,66 @@ def mandel(x=r_width, y=i_height, num_steps=20, init_value=0, max_level=255):
     return ret
 
 
+def int_mandel(x=r_width, y=i_height, num_steps=20,
+               init_value=0, max_level=255):
+    BASE = 1000000
+    MAX_NORM = BASE * BASE * 2
+    xx = np.linspace(-2*BASE, 2*BASE, x)
+    yy = np.linspace(-2*BASE, 2*BASE, x)
+    # Generate the coordinates in the complex plane
+    Y, X = np.meshgrid(xx, yy, indexing='ij')
+    # Combine them into a matrix of complex numbers
+    # Z = X + 1j * Y
+    # Retrieve the dimensions of Z
+    x_len, y_len = zs = X.shape
+    # The length in the x direction
+    # The length in the y direction
+    # value is initialized to init_value in every point of the plane
+    value_re = np.ones(zs, dtype=np.longlong) * init_value
+    value_im = np.ones(zs, dtype=np.longlong) * init_value
+    # In the beginning all points are considered as part of the Mandelbrot
+    # set. Thus, they are initialized to zero.
+    ret = np.zeros(zs, dtype=np.uint)
+    # The mask which indicates which points have already overflowed, is set
+    # to zero, to indicate that none have so far.
+    mask = np.zeros(zs, dtype=bool)
+
+    # Perform the check "num_steps" times
+    for step in range(num_steps):
+        # For every point with a mandel value of "v" and a coordinate of "z"
+        # perform  v <- (v ^ 2) + z
+        #
+        # * is an element-by-element multiplication of two matrixes
+        # of the same size.
+        # value = (value * value) + Z
+        value_re, value_im = (
+            ((value_re ** 2 - value_im ** 2) / BASE)+X,
+            ((2 * value_re * value_im) / BASE)+Y,
+        )
+        # assert value.any()
+        # Retrieve the points that overflowed in this iteration
+        # An overflowed point has a mandel value with an absolute value greater
+        # than 2.
+        current_mask = ((value_re ** 2 - value_im ** 2) > MAX_NORM)
+        # Update the mask. We use "or" in order to avoid a situation where
+        # 1 and 1 become two or so.
+        mask = np.logical_or(mask, current_mask)
+        # Upgrade the points in the mask to a greater value in the returned
+        # Mandelbrot-map.
+        ret += mask
+        # Zero the points that have overflowed, so they will not propagate
+        # to infinity.
+        value_re *= np.logical_not(current_mask)
+        value_im *= np.logical_not(current_mask)
+
+    # Now ret is ready for prime time so we return it.
+    ret = (ret * max_level) // num_steps
+    return ret
+
+
 def main():
     mandelbrot_set = mandel(max_level=255, num_steps=255)
+    # mandelbrot_set = int_mandel(max_level=255, num_steps=255)
     mandelbrot_set = mandelbrot_set.astype('uint8')
     # m = np.repeat(mandelbrot_set, 3, axis=1)
     gradient = "Tropical Colors"
