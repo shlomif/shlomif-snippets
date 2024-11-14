fa0678fac09156522cfc19f140d8286a3cb6e083
Shlomi Fish <shlomif@shlomifish.org>
2024-11-14 03:11:01 +0200

    Modernize UDP client

--- udp-client.pl
+++ udp-client.pl
@@ -4,24 +4,50 @@ use strict;
 use warnings;
 use autodie;
 
-use Socket qw( inet_aton sockaddr_in );
+use Time::HiRes qw( usleep );
+use IO::Socket  qw(  );
+use Socket      qw( SOCK_DGRAM SOCK_STREAM getaddrinfo );
 
-use IO::Socket::INET ();
-use Time::HiRes      qw( usleep );
+my $REMOTE_HOSTNAME = "localhost";
+my $PORT            = 5000;
+
+my %hints = ( socktype => SOCK_DGRAM );
+my ( $err, @res ) = getaddrinfo( $REMOTE_HOSTNAME, "${PORT}", \%hints );
+die "Cannot getaddrinfo - $err" if $err;
+
+my $sock;
+
+ADDRINFO_LOOP:
+foreach my $ai (@res)
+{
+    my $candidate = IO::Socket->new();
+
+    $candidate->socket( $ai->{family}, $ai->{socktype}, $ai->{protocol} )
+        or next ADDRINFO_LOOP;
+
+    $candidate->connect( $ai->{addr} )
+        or next ADDRINFO_LOOP;
+
+    $sock = $candidate;
+    last ADDRINFO_LOOP;
+}
+
+die "Cannot connect to localhost:echo" unless $sock;
 
 my $DELAY_IN_USECS = shift || 20_000;
 my $NUM_PACKETS    = shift || 100;
 
-my $LOCAL_INTERNET_ADDRESS = "localhost";
+my $SOCKET = $sock;
 
-my $PORT = 5000;
+if (0)
+{
+    my $LOCAL_INTERNET_ADDRESS = "localhost";
+    IO::Socket::INET->new(
+        Proto     => 'udp',
+        LocalAddr => $LOCAL_INTERNET_ADDRESS,
+    );
 
-my $SOCKET = IO::Socket::INET->new(
-    Proto     => 'udp',
-    LocalAddr => $LOCAL_INTERNET_ADDRESS,
-);
-
-my $REMOTE_HOSTNAME = "localhost";
+}
 
 STDOUT->autoflush(1);
 
@@ -31,13 +57,16 @@ $SIG{TERM} = sub {
     exit(-1);
 };
 
-my $REMOTE_INTERNET_ADDR = inet_aton($REMOTE_HOSTNAME) or die "unknown host";
-my $REMOTE_PORT_AND_ADDR = sockaddr_in( $PORT, $REMOTE_INTERNET_ADDR );
+# my $REMOTE_INTERNET_ADDR = inet_aton($REMOTE_HOSTNAME) or die "unknown host";
+# my $REMOTE_PORT_AND_ADDR = sockaddr_in( $PORT, $REMOTE_INTERNET_ADDR );
 for ( ; $count < $NUM_PACKETS ; ++$count )
 {
     my $msg = pack( "A40", sprintf( "%s", $count ) );
     print qq#Sending "$msg"!\n#;
-    defined( $SOCKET->send( $msg, 0, $REMOTE_PORT_AND_ADDR ) )
+
+    # defined( $SOCKET->send( $msg, 0, $REMOTE_PORT_AND_ADDR ) )
+    # or die "send ${REMOTE_HOSTNAME}: $!";
+    defined( $SOCKET->send( $msg, ) )
         or die "send ${REMOTE_HOSTNAME}: $!";
     if ( $DELAY_IN_USECS > 0 )
     {
